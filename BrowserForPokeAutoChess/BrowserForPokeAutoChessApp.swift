06c9fe27972cb5b933bd779358d073f9890c200c
AHNJUNGYUNG <ekdlrkzm@naver.com>
2025-06-01 12:59:10 +0900

    Initial Commit

---
+++ BrowserForPokeAutoChess/BrowserForPokeAutoChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserForPokeAutoChessApp.swift
+//  BrowserForPokeAutoChess
+//
+//  Created by 안준경 on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct BrowserForPokeAutoChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
