e19eec5e4752b8e339781ad2f69710b225f94cf8
AHNJUNGYUNG <ekdlrkzm@naver.com>
2025-06-30 14:16:03 +0900

    chore: 라이브러리 삭제로 인한 불필요 코드 삭제

--- BrowserForPokeAutoChess/BrowserForPokeAutoChessApp.swift
+++ BrowserForPokeAutoChess/BrowserForPokeAutoChessApp.swift
@@ -6,27 +6,12 @@
 //
 
 import SwiftUI
-import GoogleSignIn
 
 @main
 struct MyApp: App {
-    
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-    }
-}
-
-// AppDelegate.swift
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(
-        _ app: UIApplication,
-        open url: URL,
-        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-    ) -> Bool {
-        return GIDSignIn.sharedInstance.handle(url)
     }
 }
